7eefe497358af7e8babfbe186cddb602dafbb04e
Sungjin Han <meinside@gmail.com>
2019-07-09 09:50:37 +0900

    Remove geolocation function (use github.com/meinside/ipstack-go)

--- status/status_test.go
+++ status/status_test.go
@@ -4,20 +4,16 @@ import (
 	"testing"
 )
 
-// If this test fails, visit http://geoip.nekudo.com/ and check for any change.
-func TestGeoLocation(t *testing.T) {
-	ip := "8.8.8.8"
-	var lat float32 = 37.751
-	var lon float32 = -97.822
+func TestIpAddresses(t *testing.T) {
+	ips := IpAddresses()
+	if len(ips) <= 0 {
+		t.Errorf("failed to get local ip addresses")
+	}
+}
 
-	if geoInfo, err := GeoLocation(ip); err == nil {
-		if geoInfo.Ip != ip {
-			t.Errorf("returned ip differs from the requested one: %s", geoInfo.Ip)
-		}
-		if geoInfo.Location.Latitude != lat || geoInfo.Location.Longitude != lon {
-			t.Errorf("returned location seems to be different from the expected one: (%.3f, %.3f)", geoInfo.Location.Latitude, geoInfo.Location.Longitude)
-		}
-	} else {
-		t.Errorf("failed to get geo location: %s\n", err)
+func TestExternalIpAddress(t *testing.T) {
+	_, err := ExternalIpAddress()
+	if err != nil {
+		t.Errorf("failed to get external ip addresses: %s", err)
 	}
 }
